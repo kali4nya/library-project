e583155a6db6148bc172e7272ddc329fdd1fe9fe
kali4nya <QuBotek00@gmail.com>
2025-04-22 13:20:24 +0200

    a

--- config.py
+++ config.py
@@ -4,3 +4,6 @@ class Config:
     USERS_DIR = "users.json"
     BOOKS_DIR = "books.json"
     ENABLE_AI_BOOK_OVERVIEW = True #will not be enabled without an api key in openai_API_key.env
+    
+    BORROW_TIME_ALLOWED = "2 days" #you can use 'minute', 'minutes', 'hour', 'hours', 'day', 'days', 'week', 'weeks', 'month', 'months', 'year', 'years'. SEPERATE NUMBER FROM WORD WITH SINGLE SPACE
+    #please use just one example: "2 days" or "5 minutes" or "1 week", DON'T DO "2 hours 20 minutes" DO "140 minutes" INSTEAD (for now at least)
